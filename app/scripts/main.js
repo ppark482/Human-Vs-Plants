acee663b1b0f5f8ce32a7736e462b0e1de3dfb0d
Paul Park <ppark482@gmail.com>
2014-10-22 15:33:19 -0400

    added start button

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,30 @@
-var userName = prompt('What is your name?');
+// var Humans = {
+//   name: userName,
+// };
+//
+// var julius = new Humans () { // Creates Balanced Character Julius
+//   this.health: 150,
+//   this.attack: 10,
+//   this.defense: 10
+// };
+//
+// var george = new Humans () { // Creates Offensive Character George
+//   this.health: 200,
+//   this.attack: 15,
+//   this.defense: 5
+// };
+//
+// var barbie = new Humans () { // Creates Defensive Character Barbie
+//   this.health: 100,
+//   this.attack: 5,
+//   this.defense: 15
+// };
 
-var Humans = function () {
-  name: userName,
-};
+/* Game Start Up */
+var introTemplate = $("#intro").html(),
+    introRender = _.template(introTemplate);
 
-var julius = new Humans () { // Creates Balanced Character Julius
-  this.health: 150,
-  this.attack: 10,
-  this.defense: 10
-};
-
-var george = new Humans () { // Creates Offensive Character George
-  this.health: 200,
-  this.attack: 15,
-  this.defense: 5
-};
-
-var barbie = new Humans () { // Creates Defensive Character Barbie
-  this.health: 100,
-  this.attack: 5,
-  this.defense: 15
-};
+$('#startButton').on('click', function() {
+  // Start button on click displays intro message
+  $('.viewPort').html(introRender);
+});
