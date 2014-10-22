62ff007600a323339437cd5120812a7ac99065fd
Paul Park <ppark482@gmail.com>
2014-10-22 16:38:17 -0400

    started character picker javascript

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,30 +1,34 @@
-// var Humans = {
-//   name: userName,
-// };
-//
-// var julius = new Humans () { // Creates Balanced Character Julius
-//   this.health: 150,
-//   this.attack: 10,
-//   this.defense: 10
-// };
-//
-// var george = new Humans () { // Creates Offensive Character George
-//   this.health: 200,
-//   this.attack: 15,
-//   this.defense: 5
-// };
-//
-// var barbie = new Humans () { // Creates Defensive Character Barbie
-//   this.health: 100,
-//   this.attack: 5,
-//   this.defense: 15
-// };
+/* Creating Characters */
+var Humans = function(name, health, attack, defense) {
+  this.name = name;
+  this.health = health;
+  this.attack = attack;
+  this.defense = defense;
+};
+var julius = new Humans('Julius', 150, 10, 10); // Creates Balanced Character Julius
+var abe = new Humans('Abe', 200, 15, 5); // Creates Offensive Character Abe
+var barbie = new Humans('Barbie', 100, 5, 15); // Creates Defensive Character Barbie
+
 
 /* Game Start Up */
-var introTemplate = $("#intro").html(),
+var introTemplate = $('#intro').html(),
     introRender = _.template(introTemplate);
 
-$('#startButton').on('click', function() {
+$('#startButton').on('click', function() { // Start Button
+
   // Start button on click displays intro message
   $('.viewPort').html(introRender);
+
+  // If any key is pressed, the animation of intro message is stopped
+  $(window).keypress(function(k) {
+    if (k.keyCode > 0) {
+      $('.viewPort p').css('animation', '0');
+    };
+  });
 });
+
+/* After intro text, character picker */
+var charTemplate = $('#charPicker').html(),
+    charRender = _.template(charTemplate);
+
+$('')
